21942f0498415256f34db25e158d137b5632c816
Kenny Higgins <terrestrialabyss@gmail.com>
2025-10-25 06:33:32 -0400

    batman

---
+++ src/include/test.hpp
@@ -0,0 +1,16 @@
+//
+// Created by - on 10/25/25.
+//
+
+#ifndef TASK_CLI_TEST_HPP
+#define TASK_CLI_TEST_HPP
+
+#include <iostream>
+
+class Test {
+    public:
+    static void print() { std::println("This is a test."); }
+};
+
+
+#endif //TASK_CLI_TEST_HPP
